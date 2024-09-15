3b50710dd014bc1a7f9e327e310ed6af7086586e
avtararin <139643463+avtararin@users.noreply.github.com>
2024-09-15 12:59:00 +0300

    added concole application

--- cli.py
+++ cli.py
@@ -1,9 +1,24 @@
 from ML.model import SentenceTransformersOnnxInference
+from ML.distance import Meter
 import pandas as pd
-a = SentenceTransformersOnnxInference("ML/checkpoints/sentence_transformer.onnx", "ML/tokenizer")
+import sys
 
 
-df = pd.read_csv("data/df_embs.csv", sep=',')
-print(len(df.T[0].tolist()[1:]))
-print(df.loc[:, 'tag'].tolist()[0])
-print(df.T[0].tolist()[1:])
+def find_nearest_cluster(tag):
+    # инициализация модели
+    model = SentenceTransformersOnnxInference("ML/checkpoints/sentence_transformer.onnx", "ML/tokenizer")
+    centroids = pd.read_csv("data/kmeans_centroids.csv", sep=" , ").to_numpy()
+    meter = Meter()
+    tag_emb = model.get_embedding(tag)
+    min_dist = meter.get_distance(tag_emb, centroids[0])
+    nearest_centroid = centroids[0]
+    for centroid in centroids:
+        distance = meter.get_distance(tag_emb, centroid)
+        if distance < min_dist:
+            min_dist = distance
+            nearest_centroid = centroid
+    return nearest_centroid
+
+
+if __name__ == "__main__":
+    print(find_nearest_cluster(sys.argv[1]))
